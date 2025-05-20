074db5111a4beeebeae136cb962440d1410eb2bb
Hemant Kumar <hemantkumar08.dhn@gmail.com>
2025-05-20 22:11:11 +0530

    final

--- frontend/app.py
+++ frontend/app.py
@@ -12,12 +12,12 @@ if choice == "Upload":
     uploaded_file = st.file_uploader("Upload a document") 
     if uploaded_file: 
         files = {"file": (uploaded_file.name, uploaded_file, uploaded_file.type)} 
-        res = requests.post("{BACKEND_URL}/upload", files=files) 
+        res = requests.post(f"{BACKEND_URL}/upload", files=files) 
         if res:
             st.markdown("## 📄File Uploaded Successfully ")
 
 elif choice == "Documents": 
-    res = requests.get("{BACKEND_URL}/documents") 
+    res = requests.get(f"{BACKEND_URL}/documents") 
     docs = res.json()
     st.markdown("### 📄 Uploaded Documents")
     for doc in docs:
@@ -28,7 +28,7 @@ elif choice == "Chat":
     doc_id = st.text_input("Enter Document ID")
     question = st.text_input("Ask a question") 
     if doc_id and question: 
-        res = requests.post("{BACKEND_URL}/chat", json={"doc_id":doc_id, "question": question}) 
+        res = requests.post(f"{BACKEND_URL}/chat", json={"doc_id":doc_id, "question": question}) 
         
         if res.status_code == 200:
             response_text = res.text
@@ -38,7 +38,7 @@ elif choice == "Chat":
 elif choice == "Themes": 
     question = st.text_input("Ask question")
     if question:
-        res = requests.post("{BACKEND_URL}/themes", json={"question":question}) 
+        res = requests.post(f"{BACKEND_URL}/themes", json={"question":question}) 
         if res.status_code == 200:
             response_text = res.text
             formated_text = response_text.replace("\\n","\n")
