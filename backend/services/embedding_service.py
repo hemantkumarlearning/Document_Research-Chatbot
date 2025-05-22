17f14d544f78e07fb4b535aa7311fdfc6297ad30
Hemant Kumar <hemantkumar08.dhn@gmail.com>
2025-05-22 11:49:33 +0530

    updated

--- backend/services/embedding_service.py
+++ backend/services/embedding_service.py
@@ -1,9 +1,13 @@
-from sentence_transformers import SentenceTransformer 
+# from sentence_transformers import SentenceTransformer 
 
 #Load model
-model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
+# model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
+model = ""
 
 def embed_text(text):
 
+    if model:
     #Encoding text into vector representation
-    return model.encode(text)
+        return model.encode(text)
+    else:
+        return
